7e609a7ff0a3cdd3c18120856b12a7a291645f6d
Geovanny Reyes <geovanny.reyes@correounivalle.edu.co>
2022-08-14 10:05:28 -0500

    chat

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import io from 'socket.io-client';
+
+
+const CHAT_SOCKET_ENDPOINT = 'http://localhost:3000';
 
 @Component({
   selector: 'app-chat',
@@ -6,10 +10,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat.component.css']
 })
 export class ChatComponent implements OnInit {
+  socket: any;
+  message: any;
+  @ViewChild('card_content') card_content: ElementRef<HTMLDivElement> | any;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.setUpSocketConnection();
   }
 
+  setUpSocketConnection() {
+    this.socket = io(CHAT_SOCKET_ENDPOINT);
+    this.socket.on('msg', (message: string) => {
+      if (message) {
+        const item = document.createElement('div');
+        item.innerHTML = `@user: ${message}`;
+        item.className = '';
+        document.getElementsByClassName('card-content')[0].appendChild(item);
+      }
+    });
+  }
+
+  SendMessage() {
+    this.socket.emit('message', this.message);
+    this.message = "";
+  }
 }
